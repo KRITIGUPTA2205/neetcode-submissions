38e8a64e041edf18c2e1da09cb955c5f940874f7
Kriti Gupta <156075114+KRITIGUPTA2205@users.noreply.github.com>
2026-04-22 12:56:50 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l=0;
+        int r=1;
+        int p=0;
+        while(r<prices.length){
+            if(prices[l]<prices[r]){
+                p=Math.max(p,prices[r]-prices[l]);
+            }else{
+                l=r;
+            }
+            r++;
+        }
+        return p;
+    }
+}
